017fde321575f7758d8f6f3304c9bcc01cb6a570
Carl Peaslee <cpeaslee@gmail.com>
2017-01-09 15:24:45 -0600

    integrated material-ui

--- src/containers/Template.js
+++ src/containers/Template.js
@@ -1,20 +1,26 @@
 import React, {Component} from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+
+injectTapEventPlugin()
 
 class Template extends Component {
 
   render () {
     return (
-      <div>
-        <header>
-          <h1>TicTacTuring</h1>
-        </header>
-        <main>
-          {this.props.children}
-        </main>
-        <footer>
-          <i>Copyright Carl Peaslee</i>
-        </footer>
-      </div>
+      <MuiThemeProvider>
+        <div>
+          <header>
+            <h1>TicTacTuring</h1>
+          </header>
+          <main>
+            {this.props.children}
+          </main>
+          <footer>
+            <i>Copyright Carl Peaslee</i>
+          </footer>
+        </div>
+      </MuiThemeProvider>
     )
   }
 }
