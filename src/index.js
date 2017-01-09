2279374af37989951340a35712770d0a4fadf963
Carl Peaslee <cpeaslee@gmail.com>
2017-01-09 10:31:43 -0600

    deleted unneeded files and modifed index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <h2>TicTacTuring</h2>
+  </div>,
   document.getElementById('root')
 );
