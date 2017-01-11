5462cf9534d951dbbea92f9143756d2ad369e9bf
Carl Peaslee <cpeaslee@gmail.com>
2017-01-11 08:08:05 -0600

    0303

--- src/index.js
+++ src/index.js
@@ -1,11 +1,46 @@
 /* @flow */
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, browserHistory} from 'react-router'
+import {Router, browserHistory, applyRouterMiddleware} from 'react-router'
 import Routes from './routes'
+import Relay from 'react-relay'
+import useRelay from 'react-router-relay'
+import {
+  RelayNetworkLayer,
+  urlMiddleware
+} from 'react-relay-network-layer'
+
+const createHeaders = (idToken) => {
+  if (idToken) {
+    return {
+      Authorization: 'Bearer ' + idToken
+    }
+  } else {
+    return {}
+  }
+}
+
+Relay.injectNetworkLayer(
+  new RelayNetworkLayer([
+    urlMiddleware({
+      url: (req) => process.env.REACT_APP_GRAPHQL_URL,
+    }),
+    next => req => {
+      let idToken = localStorage.getItem('idToken')
+      let headers = createHeaders(idToken)
+      req.headers = {
+        ...req.headers,
+        ...headers
+      }
+      return next(req)
+    },
+  ],{ disableBatchQuery: true })
+)
 
 ReactDOM.render(
   <Router
+    environment={Relay.Store}
+    render={applyRouterMiddleware(useRelay)}
     history={browserHistory}
     routes={Routes}
   />,
